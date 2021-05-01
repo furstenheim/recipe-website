25c1f5221a1bf3032b6e0c9e34ec085f78ddf001
Gabriel <furstenheim@gmail.com>
2021-05-01 15:30:55 +0200

    Use div inside link to make it clickable

--- src/RecipeSummaryPreview.svelte
+++ src/RecipeSummaryPreview.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">//
-import { push } from 'svelte-spa-router'
 import SvelteMarkdown from 'svelte-markdown'
 import type { RecipeSummary } from './recipe'
 
 export let recipe: RecipeSummary = null
-let hovered = false
-function onMouseEnter () {
-  hovered = true
-}
 
-function onMouseLeave () {
-  hovered = false
-  console.log('hovered', hovered)
-}
 </script>
 
-<div class="summary" on:click={() => push(`/recipe/${recipe.id}`)} on:mouseenter={onMouseEnter} on:mouseleave={onMouseLeave} class:hovered={hovered}>
-  <SvelteMarkdown source="{recipe.title}" />
-  <SvelteMarkdown source="{recipe.img}" />
-</div>
+
+<a href="/#/recipe/{recipe.id}">
+
+  <div class="summary" >
+    <SvelteMarkdown source="{recipe.title}" />
+    <SvelteMarkdown source="{recipe.img}" />
+  </div>
+
+</a>
+
 
 <style>
-  .hovered {
-      cursor: pointer
+  :global(.summary) {
+      text-decoration: none;
+      color: black;
   }
-
   :global(.summary img) {
      max-width: 500px;
   }
